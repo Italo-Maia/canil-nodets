5dc796635c52d27518497dd73165c724b9a03c90
Italo-Maia <italomaiatech@gmail.com>
2022-07-30 15:52:36 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # canil-nodets
+Projeto feito no modulo do curso de Node + Typescript
+
+### Pré-requistos globais
+`npm install -g nodemon typesrcipt ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o projeto
+`npm run start-dev`
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+
+export const home =  (req: Request, res: Response) => {
+
+    //res.sen(pages/home)
+}
+
+export const dogs =  (req: Request, res: Response) => {
+
+    //res.sen(pages/home)
+}
+
+export const cats =  (req: Request, res: Response) => {
+
+    //res.sen(pages/home)
+}
+
+export const fishes =  (req: Request, res: Response) => {
+
+    //res.sen(pages/home)
+}
+
